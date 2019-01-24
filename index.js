377801591ae8aa6b253076bcc2226fdaec0c0045
Moussa Ndour <moussa_ndour@hotmail.fr>
2019-01-24 00:56:49 +0000

    deployt

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
 const welcome = `Hello word`;
-let accessToken = 'EAAFNZCjk90OIBADjpXdZAeTZC0D518D13p7ZBEDyU5sY4GeVZBAEcyreTcZAxpiXczXZAbUwruNVHIThMFHp1IjF5ZCmOHSW4lqllOZADGsDuMF9O3QZAZAFWWuWf7RvcaUlKmHYLCFr8eRvuN6wpzYrOMko0tYfgtPd81z9ynEDaKmVQZDZD';
-
+const accessToken = 'EAAFNZCjk90OIBADjpXdZAeTZC0D518D13p7ZBEDyU5sY4GeVZBAEcyreTcZAxpiXczXZAbUwruNVHIThMFHp1IjF5ZCmOHSW4lqllOZADGsDuMF9O3QZAZAFWWuWf7RvcaUlKmHYLCFr8eRvuN6wpzYrOMko0tYfgtPd81z9ynEDaKmVQZDZD';
+const verifyToken = '84d46ce2432d2308c8f3905d845afde7';
 const express = require('express')
 const app = express()
 const bodyParser = require('body-parser'); // parser les requete POST en JSON ou url-encoded,....
@@ -9,7 +9,18 @@ const port = process.env.PORT || 3000; // sur heroku un port nous ai donne autom
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded());
 
-app.get('/', (req, res) => res.send('Hello World!'))
+
+(async () => {
+
+  app.get('/', (req, res) => res.send('Hello World!'))
+
+  app.post('/webhook', async(req, body) => {
+    let data = req.body;
+     console.console.log(data);
+  })
+
+
+})()
 
 app.listen(port, () => console.log(`Example app listening on port ${port}!`))
 
