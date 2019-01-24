229e414626a378cb06e83ad8a6637b96ad990c15
Moussa Ndour <moussa_ndour@hotmail.fr>
2019-01-24 01:03:00 +0000

    deploy

--- index.js
+++ index.js
@@ -12,7 +12,32 @@ app.use(bodyParser.urlencoded());
 
 (async () => {
 
-  app.get('/', (req, res) => res.send('Hello World!'))
+  app.get('/', (req, res) => res.send('Hello World!'));
+
+  app.get('/webhook', (req, res) => {
+
+  // Your verify token. Should be a random string.
+  // Parse the query params
+  let mode = req.query['hub.mode'];
+  let token = req.query['hub.verify_token'];
+  let challenge = req.query['hub.challenge'];
+
+  // Checks if a token and mode is in the query string of the request
+  if (mode && token) {
+
+    // Checks the mode and token sent is correct
+    if (mode === 'subscribe' && token === verifyToken) {
+
+      // Responds with the challenge token from the request
+      console.log('WEBHOOK_VERIFIED');
+      res.status(200).send(challenge);
+
+    } else {
+      // Responds with '403 Forbidden' if verify tokens do not match
+      res.sendStatus(403);
+    }
+  }
+});
 
   app.post('/webhook', async(req, res) => {
     let data = req.body;
